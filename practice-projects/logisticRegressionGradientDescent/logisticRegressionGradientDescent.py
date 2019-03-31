fd6706e002c1188bf3b7822f33b47bc429388767
mdbeauche <michael.beauchemin@gmail.com>
2019-03-31 12:53:52 -0700

    chore: refactor to be more useful for testing later

--- practice-projects/logisticRegressionGradientDescent/logisticRegressionGradientDescent.py
+++ practice-projects/logisticRegressionGradientDescent/logisticRegressionGradientDescent.py
@@ -4,66 +4,61 @@ import scipy
 from scipy import ndimage
 from PIL import Image
 
-def sigmoid(x):
-    return (1./(1. + np.exp(-x)))
-
-# Binary Image Classification with Logistic Regression using Gradient Descent
+"""
+Binary Image Classification with Logistic Regression and Gradient Descent
+assumption: all images in training and test sets have the same height and width
+"""
 class LogisticRegressionGD:
-    def __init__(self, X_train, Y_train, X_test, Y_test, classes):
+    def __init__(self, classes, imageWidth, imageHeight):
         """
-        X_train: training set represented by a numpy array of shape (m_train,width,height,3[RGB])
-        Y_train: training labels (classification) represented by a numpy array of shape (m_train,)
-        X_test: test set represented by a numpy array of shape (m_test,width,height,3[RGB])
-        Y_test: test labels represented by a numpy array of shape (m_test,)
         classes: list of classes (set of labels)
+        imageHeight: the height of all images
+        imageWidth: the width of all images
         """
-        # mark width and height of training images before flattening set
-        self.width = X_train.shape[1]
-        self.height = X_train.shape[2]
-
-        # training set
-        # flatten to 1d array and standardize input
-        X_train = (X_train.reshape(X_train.shape[0], -1).T) / 255
-        X_test = (X_test.reshape(X_test.shape[0], -1).T) / 255
-        self.X_train = X_train # numpy array of shape (width * height * 3, m_train)
-        self.X_test = X_test # numpy array of shape (width * height * 3, m_test)
-
-        # training labels
-        # flatten
-        Y_train = Y_train.reshape((1, Y_train.shape[0]))
-        Y_test = Y_test.reshape((1, Y_test.shape[0]))
-        self.Y_train = Y_train # numpy array of shape (1, m_train)
-        self.Y_test = Y_test # numpy array of shape (1, m_test)
-
-        # sanity check
-        assert(X_train.shape[1] == Y_train.shape[1])
-        assert(X_test.shape[1] == Y_test.shape[1])
-
-        # m training examples
-        self.m_train = X_train.shape[1]
-        # m test examples
-        self.m_test = X_test.shape[1]
+        self.width = imageWidth
+        self.height = imageHeight
 
         # classes
         self.classes = classes
 
         # initialize with zeros
         # weights: a numpy array of size (width * height * 3, 1)
-        self.weights = np.zeros((X_train.shape[0],1))
+        self.weights = np.zeros((imageWidth*imageHeight*3,1))
         # bias: a scalar
         self.bias = 0.
 
         # keep track of cost of each iteration
         self.costs = []
 
+        self.trainingDataLoaded = False
+
+    def loadTrainingData(self, X_train, Y_train):
+        """
+        X_train: training images represented by a numpy array of shape (m_train,width,height,3[RGB])
+        Y_train: training labels (classification) represented by a numpy array of shape (m_train,)
+        """
+        # sanity check
+        assert(X_train.shape[1] == self.width)
+        assert(X_train.shape[2] == self.height)
+
+        self.X_train, self.Y_train = loadData(X_train, Y_train)
+
+        # m training examples
+        self.m_train = self.X_train.shape[1]
+
+        self.trainingDataLoaded = True
+
     def train(self, num_iterations = 2000, learning_rate = 0.005):
         """
-        num_iterations -- number of iterations of the optimization loop
-        learning_rate -- learning rate of the gradient descent update rule
+        num_iterations: number of iterations of the optimization loop
+        learning_rate: learning rate of the gradient descent update rule
         """
+        if self.trainingDataLoaded == False:
+            return {}
+
         # train: learn (optimize) weights and bias
         for i in range(num_iterations):
-            # Cost and gradient calculation
+            # cost and gradient calculation
             # forward propagation (from x to cost)
             # Z = w.T*X + b
             Z = np.dot(self.weights.T, self.X_train) + self.bias
@@ -89,34 +84,70 @@ class LogisticRegressionGD:
 
         # measure prediction on training set
         A = sigmoid(np.dot(self.weights.T, self.X_train) + self.bias)
-        Y_prediction_train = np.where(A > 0.5, 1, 0)
-
-        # measure prediction on test set
-        A = sigmoid(np.dot(self.weights.T, self.X_test) + self.bias)
-        Y_prediction_test = np.where(A > 0.5, 1, 0)
+        Y_prediction_train = np.where(A >= 0.5, 1, 0)
 
         training_accuracy = 100 - np.mean(np.abs(Y_prediction_train - self.Y_train)) * 100
-        test_accuracy = 100 - np.mean(np.abs(Y_prediction_test - self.Y_test)) * 100
 
         return { "costs": self.costs,
-                 "Y_prediction_test": Y_prediction_test,
                  "Y_prediction_train" : Y_prediction_train,
                  "weights" : self.weights,
                  "bias" : self.bias,
-                 "training_accuracy": training_accuracy,
+                 "training_accuracy": training_accuracy }
+
+    def test(self, X_test, Y_test):
+        # sanity check
+        assert(X_test.shape[1] == self.width)
+        assert(X_test.shape[2] == self.height)
+
+        X_test, Y_test = loadData(X_test, Y_test)
+
+        # measure prediction on test set
+        A = sigmoid(np.dot(self.weights.T, X_test) + self.bias)
+        Y_prediction_test = np.where(A >= 0.5, 1, 0)
+
+        test_accuracy = 100 - np.mean(np.abs(Y_prediction_test - Y_test)) * 100
+
+        return { "Y_prediction_test" : Y_prediction_test,
                  "test_accuracy": test_accuracy }
 
     def predict(self, imageFile):
         # read image into np array
         image = np.array(ndimage.imread(imageFile, flatten=False))
         # resize image to match training examples, flatten
-        reshape_image = scipy.misc.imresize(image, size=(self.width,self.height)).reshape((1, self.weights.shape[0])).T
+        resize_image = scipy.misc.imresize(image, size=(self.width,self.height))
+        reshape_image = resize_image.reshape((1, self.weights.shape[0])).T
         # compute activation
         A = sigmoid(np.dot(self.weights.T, reshape_image) + self.bias)
         # compute prediction
-        Y = np.where(A > 0.5, 1, 0)
+        Y = np.where(A >= 0.5, 1, 0)
         classification = np.squeeze(Y)
-        print("Predicted classification: " + classes[np.squeeze(Y)].decode("utf-8"))
+        print("Predicted classification: " + classes[classification].decode("utf-8"))
+
+
+def sigmoid(x):
+    return (1./(1. + np.exp(-x)))
+
+
+def loadData(X, Y):
+    """
+    X: image set represented by a numpy array of shape (m_test,width,height,3[RGB])
+    Y: labels represented by a numpy array of shape (m_test,)
+    """
+    # image set
+    # flatten to 1d array and standardize input
+    X = (X.reshape(X.shape[0], -1).T) / 255
+    # numpy array of shape (width * height * 3, m_train)
+
+    # labels
+    # reshape
+    Y = Y.reshape((1, Y.shape[0]))
+    # numpy array of shape (1, m_train)
+
+    # sanity check
+    assert(X.shape[1] == Y.shape[1])
+
+    return X, Y
+
 
 if __name__ == "__main__":
     train_dataset = h5py.File("datasets/train_catvnoncat.h5", "r")
@@ -133,9 +164,15 @@ if __name__ == "__main__":
 
     classes = np.array(test_dataset["list_classes"][:])
 
-    model = LogisticRegressionGD(train_set_x, train_set_y, test_set_x, test_set_y, classes)
-    results = model.train()
+    width = train_set_x.shape[1]
+    height = train_set_x.shape[2]
 
-    print("Training accuracy: %f, test accuracy: %f" %(results["training_accuracy"], results["test_accuracy"]))
+    model = LogisticRegressionGD(classes, width, height)
+    model.loadTrainingData(train_set_x, train_set_y)
+    trainingResults = model.train()
+
+    testResults = model.test(test_set_x, test_set_y)
+
+    print("Training accuracy: %f, test accuracy: %f" %(trainingResults["training_accuracy"], testResults["test_accuracy"]))
 
     model.predict("datasets/test_image.jpg")
